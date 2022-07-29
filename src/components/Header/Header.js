c88053c2685e0eca2df66e0fe205790ee57d4037
Pere Afezu <yodas.elite@gmail.com>
2022-07-29 17:10:53 +0100

    finished dreamflo

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css'
+
+function Header() {
+  return (
+    <div className="Header-wrapper">
+      <h1>DreamFlow</h1>
+      <span className='love' >🤗</span>
+      <h5>Drag and drop completed dreams !</h5>
+    </div>
+  )
+}
+
+export default Header;
